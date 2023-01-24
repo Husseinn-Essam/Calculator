bf49566b2e7b0dc2947707ebee792964a030da04
Husseinn-Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2023-01-24 14:34:16 +0200

    Can chain adds without reseting mode

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const subKey = document.getElementById("sub");
 const multiKey = document.getElementById("multi");
 const diviKey = document.getElementById("divi");
 const equalKey = document.getElementById("equal");
+const disp = document.getElementById("display");
 let numKey = document.querySelectorAll(".num");
 
 let Arr = []
@@ -26,26 +27,29 @@ let mode;
 for (var i = 0; i < 10; i++) {
     numKey[i].addEventListener('click', (e) => {
         Arr.push(parseInt(e.target.textContent))
-
+        a = Arr.reduce((accum, digit) => {
+            return (accum * 10) + digit
+        }, 0)
+        console.log(a);
+        disp.innerText = a;
     })
 }
 
 addKey.addEventListener('click', () => {
     if (!mode) {
-        a = Arr.reduce((accum, digit) => {
-            return (accum * 10) + digit
-        }, 0)
-        //console.log(a);
+
         Arr.length = 0;
         mode = "add";
-        opArr.push(a);
+        if (opArr == 0) {
+            opArr.push(a);
+        }
         console.log(opArr);
+
     }
-    else {
+    else if (!mode && opArr != 0) {
         a = Arr.reduce((accum, digit) => {
             return (accum * 10) + digit
         }, 0)
-        //console.log(a);
         Arr.length = 0;
         mode = "add";
         opArr.push(a);
@@ -54,6 +58,24 @@ addKey.addEventListener('click', () => {
         console.log(result);
         opArr.length = 0;
         opArr.push(result);
+        disp.innerText = result;
+
+    }
+    else {
+        a = Arr.reduce((accum, digit) => {
+            return (accum * 10) + digit
+        }, 0)
+        disp.innerText = a;
+        Arr.length = 0;
+        mode = "add";
+        opArr.push(a);
+        console.log(opArr);
+        result = add(opArr[0], opArr[1]);
+        console.log(result);
+        opArr.length = 0;
+        opArr.push(result);
+        disp.innerText = result;
+
     }
 
 })
