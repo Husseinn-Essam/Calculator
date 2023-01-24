61967b829866732b005270859ea60c02fde13d7e
Husseinn-Essam <112550601+Husseinn-Essam@users.noreply.github.com>
2023-01-24 13:54:31 +0200

    can chain add

--- script.js
+++ script.js
@@ -18,17 +18,42 @@ const diviKey = document.getElementById("divi");
 const equalKey = document.getElementById("equal");
 let numKey = document.querySelectorAll(".num");
 
-let aArr = []
+let Arr = []
+let opArr = []
 let a;
 
+let mode;
 for (var i = 0; i < 10; i++) {
     numKey[i].addEventListener('click', (e) => {
-        aArr.push(parseInt(e.target.textContent))
-        a = aArr.reduce((accum, digit) => {
-            return (accum * 10) + digit
-        }, 0)
-        console.log(a);
+        Arr.push(parseInt(e.target.textContent))
+
     })
 }
 
+addKey.addEventListener('click', () => {
+    if (!mode) {
+        a = Arr.reduce((accum, digit) => {
+            return (accum * 10) + digit
+        }, 0)
+        //console.log(a);
+        Arr.length = 0;
+        mode = "add";
+        opArr.push(a);
+        console.log(opArr);
+    }
+    else {
+        a = Arr.reduce((accum, digit) => {
+            return (accum * 10) + digit
+        }, 0)
+        //console.log(a);
+        Arr.length = 0;
+        mode = "add";
+        opArr.push(a);
+        console.log(opArr);
+        result = add(opArr[0], opArr[1]);
+        console.log(result);
+        opArr.length = 0;
+        opArr.push(result);
+    }
 
+})
